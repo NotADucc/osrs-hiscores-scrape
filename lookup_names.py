041c7e0275440c27af612b71f586ac439f4ea4e4
Guillaume Desktop <robertohelper@outlook.com>
2025-02-16 23:01:53 +0100

    feature: finished

--- lookup_names.py
+++ lookup_names.py
@@ -9,19 +9,19 @@ def main(in_file, out_file, hs_nr):
     names = []
     with open(in_file, "r") as f:
         for line in f:
-            _, value = line.strip().split(":", 1)
-            names.append(value)
+            idx, name = line.strip().split(":", 1)
+            names.append((idx, name))
             
     hs_idx = hs_nr - 1     
     if hs_idx < 0 or hs_idx > len(names) :
         return
 
     with open(out_file, "a") as ff:
-        for index, val in enumerate(names[hs_idx:], start=hs_idx):
+        for index, (idx, name) in enumerate(names[hs_idx:], start=hs_idx):
             retries, max_retries = 0, 3
             while retries < max_retries:
                 try:
-                    csv = lookup(val).split(b'\n')
+                    csv = lookup(name).split(b'\n')
                     att = int(csv[HSCSVMapper.attack.value].split(b',')[1])
                     de = int(csv[HSCSVMapper.defence.value].split(b',')[1])
                     st = int(csv[HSCSVMapper.strength.value].split(b',')[1])
@@ -29,21 +29,20 @@ def main(in_file, out_file, hs_nr):
                     ra = int(csv[HSCSVMapper.ranged.value].split(b',')[1])
                     pr = int(csv[HSCSVMapper.prayer.value].split(b',')[1])
                     ma = int(csv[HSCSVMapper.magic.value].split(b',')[1])
-
                     cmb_lvl = calc_cmb(att, de, st, hp, ra, pr, ma)
                     if cmb_lvl < 40:
-                        ff.write('%s:%s cmb %s\n' % (index + 1, val, cmb_lvl))
+                        ff.write('%s:%s cmb %s\n' % (idx, name, cmb_lvl))
 
-                    print(f'finished nr: {index + 1} - {val}')
+                    print(f'finished nr: {idx} - {name}')
                     break 
                 except Exception as err:
                     print(err)
-                    print(f"Error occurred at index {index}: {type(err)}. Retrying...")
+                    print(f"Error occurred at nr {idx}: {type(err)}. Retrying...")
                     retries += 1
                     sleep(10)
                     if retries == max_retries:
                         with open(out_file + '.err', "a") as fff:
-                            fff.write('COULD NOT FIND %s:%s\n' % (index + 1, val))
+                            fff.write('COULD NOT FIND %s:%s\n' % (idx, name))
                         print('max retries reached')
                         break
         
--- lookup_names_scrape.py
+++ lookup_names_scrape.py
@@ -2,49 +2,54 @@ import argparse
 import sys
 
 from time import sleep
-from common import HSCSVMapper, HSLookup, calc_cmb
+from common import HSLookup, calc_cmb
 from request import lookup_scrape, extract_stats
 
 def main(in_file, out_file, hs_nr):
     names = []
     with open(in_file, "r") as f:
         for line in f:
-            _, value = line.strip().split(":", 1)
-            names.append(value)
+            idx, name = line.strip().split(":", 1)
+            names.append((idx, name))
             
     hs_idx = hs_nr - 1     
     if hs_idx < 0 or hs_idx > len(names) :
         return
 
     with open(out_file, "a") as ff:
-        for index, val in enumerate(names[hs_idx:], start=hs_idx):
+        for index, (idx, name) in enumerate(names[hs_idx:], start=hs_idx):
             retries, max_retries = 0, 3
             while retries < max_retries:
                 try:
-                    extracted_stats = extract_stats
-                   
-                    att = extracted_stats.get('Attack', 1)
-                    de = extracted_stats.get('Defence', 1)
-                    st = extracted_stats.get('Strength', 1)
-                    hp = extracted_stats.get('Hitpoints', 10)
-                    ra = extracted_stats.get('Ranged', 1)
-                    pr = extracted_stats.get('Prayer', 1)
-                    ma = extracted_stats.get('Magic', 1)
-
+                    page = lookup_scrape(name, HSLookup.pure)
+                    extracted_stats = extract_stats(page)
+                    
+                    if extracted_stats.get('TzKal-Zuk', 0) == 0 :
+                        print(f'finished nr: {idx} - {name}')
+                        break
+                        
+                    att = extracted_stats.get('Attack', {'lvl': 1})['lvl']
+                    de = extracted_stats.get('Defence', {'lvl': 1})['lvl']
+                    st = extracted_stats.get('Strength', {'lvl': 1})['lvl']
+                    hp = extracted_stats.get('Hitpoints', {'lvl': 10})['lvl']
+                    ra = extracted_stats.get('Ranged', {'lvl': 1})['lvl']
+                    pr = extracted_stats.get('Prayer', {'lvl': 1})['lvl']
+                    ma = extracted_stats.get('Magic', {'lvl': 1})['lvl']
+                    
                     cmb_lvl = calc_cmb(att, de, st, hp, ra, pr, ma)
                     if cmb_lvl < 40:
-                        ff.write('%s:%s cmb %s\n' % (index + 1, val, cmb_lvl))
+                        ff.write('%s:%s cmb %s\n' % (idx, name, cmb_lvl))
 
-                    print(f'finished nr: {index + 1} - {val}')
+                    print(f'finished nr: {idx} - {name}')
                     break 
                 except Exception as err:
                     print(err)
-                    print(f"Error occurred at index {index}: {type(err)}. Retrying...")
+                    print(f"Error occurred at nr {idx}: {type(err)}. Retrying...")
                     retries += 1
                     sleep(10)
                     if retries == max_retries:
                         with open(out_file + '.err', "a") as fff:
-                            fff.write('COULD NOT FIND %s:%s\n' % (index + 1, val))
+                            fff.write('COULD NOT FIND %s:%s\n' % (idx, name))
                         print('max retries reached')
                         break
         
