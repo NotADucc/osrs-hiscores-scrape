a5513fcdcb9461f71a5adc31da5f960465615714
Guillaume Desktop <robertohelper@outlook.com>
2025-03-22 21:41:14 +0100

    feature: added a sort script

---
+++ sort.py
@@ -0,0 +1,22 @@
+import argparse
+import sys
+
+def main(in_file):
+    with open(in_file, "r") as file:
+        lines = file.readlines()
+
+    sorted_lines = sorted(lines, key=lambda x: int(x.split(",")[0]))
+
+    with open(in_file, "w") as file:
+        file.writelines(sorted_lines)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in-file', required=True)
+    args = parser.parse_args()
+
+    main(args.in_file)
+
+    print("done")
+    sys.exit(0)
