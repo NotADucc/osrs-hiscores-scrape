fe69b63bd96190b97a8734b713399d4e892cad73
Guillaume Laptop <robertohelper@outlook.com>
2025-04-02 12:41:08 +0200

    feature: added cancellation param for spawn_threads
    BUG: cancellation doesnt do anything atm

--- util/threading_handler.py
+++ util/threading_handler.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import functools
 
 
-def spawn_threads(process, records, max_workers: int = 10, **args: dict) -> None:
+def spawn_threads(process, records, cancellation: list[bool] = [False], max_workers: int = 10, **args: dict) -> None:
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         new_process = functools.partial(
             process, **args) if args is not None else process
