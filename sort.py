d87c53d4768c4e7fc44987ed7c6a6985387bb724
Guillaume Desktop <robertohelper@outlook.com>
2025-05-11 13:10:41 +0200

    feature: added delim to sort

--- sort.py
+++ sort.py
@@ -5,11 +5,11 @@ from util.log import get_logger
 logger = get_logger()
 
 
-def main(in_file):
+def main(in_file, delimiter):
     with open(in_file, "r") as file:
         lines = file.readlines()
 
-    sorted_lines = sorted(lines, key=lambda x: int(x.split(",")[0]))
+    sorted_lines = sorted(lines, key=lambda x: int(x.split(delimiter)[0]))
 
     with open(in_file, "w") as file:
         file.writelines(sorted_lines)
@@ -18,9 +18,10 @@ def main(in_file):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-file', required=True)
+    parser.add_argument('--delimiter', default=',')
     args = parser.parse_args()
 
-    main(args.in_file)
+    main(args.in_file, args.delimiter)
 
     logger.info("done")
     sys.exit(0)
