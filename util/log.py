7ce4578caceefd92daa7e48960709cb90b5ca9e8
Guillaume Desktop <robertohelper@outlook.com>
2025-08-16 12:28:10 +0200

    feature: color text for logger

--- util/log.py
+++ util/log.py
@@ -4,12 +4,32 @@ import sys
 logger = None
 
 
-def setup_custom_logger() -> logging:
-    formatter = logging.Formatter(
-        fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+def setup_custom_logger() -> logging:
     handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    handler.setFormatter(CustomFormatter())
 
     logger = logging.getLogger('logger')
     logger.setLevel(logging.DEBUG)
