ae4067f0974de96e66140172e9ce60eef447d3da
Guillaume Desktop <robertohelper@outlook.com>
2025-08-19 16:41:39 +0200

    feature: added encoder to IO

--- src/util/io.py
+++ src/util/io.py
@@ -9,10 +9,11 @@ from src.request.errors import FinishedScript
 from src.request.results import CategoryRecord
 from src.util import json_wrapper
 
+ENCODING = "utf-8"
 
 async def write_records(in_queue: asyncio.Queue, out_file: str, format: Callable, total: int):
     exists = os.path.isfile(out_file)
-    with open(out_file, mode='w' if not exists else 'a') as f:
+    with open(out_file, mode='w' if not exists else 'a', encoding=ENCODING) as f:
         for _ in tqdm(range(total), smoothing=0.01, desc=f'writing to {out_file}'):
             job = await in_queue.get()
             if job is not None:
@@ -23,13 +24,13 @@ async def write_records(in_queue: asyncio.Queue, out_file: str, format: Callable
 
 def write_record(out_file: str, data: str):
     exists = os.path.isfile(out_file)
-    with open(out_file, mode='w' if not exists else 'a') as f:
+    with open(out_file, mode='w' if not exists else 'a', encoding=ENCODING) as f:
         f.write(data + '\n')
 
 
 def read_proxies(proxy_file: str) -> list[str]:
     if proxy_file is not None and os.path.isfile(proxy_file):
-        with open(proxy_file, "r") as f:
+        with open(proxy_file, "r", encoding=ENCODING) as f:
             proxies = f.read().splitlines()
     else:
         proxies = []
@@ -41,7 +42,7 @@ def read_hs_records(file: str) -> Iterator[CategoryRecord]:
     if not os.path.isfile(file):
         return iter([])
 
-    with open(file, "r") as f:
+    with open(file, "r", encoding=ENCODING) as f:
         for line in f:
             line = line.strip()
             if not line:
