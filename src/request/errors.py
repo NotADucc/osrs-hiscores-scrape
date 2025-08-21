619316f5b8aa6a6d88cce63498967d73a04754b3
Guillaume Desktop <robertohelper@outlook.com>
2025-08-21 17:03:57 +0200

    feature: added docstring for requests errors

--- src/request/errors.py
+++ src/request/errors.py
@@ -1,44 +1,44 @@
 class RequestFailed(Exception):
+    """ Raised when an HTTP request fails. """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class IsRateLimited(Exception):
+    """ Raised when data for a request responds with rate limit message. """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class NotFound(Exception):
-    def __init__(self, message: str, details=None):
-        self.details = details
-        super().__init__(message)
-
-
-class NoProxyList(Exception):
+    """ Raised when data for a request does not exist. """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class ParsingFailed(Exception):
+    """ Raised when data from a request cannot be parsed into the right format. """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class RetryFailed(Exception):
+    """ Raised when method/coroutine reaches . """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class ServerBusy(Exception):
+    """ Raised when the server is too busy to process a request (timeouts). """
     def __init__(self, message: str, details=None):
         self.details = details
         super().__init__(message)
 
 
 class FinishedScript(Exception):
-    pass
+    """ Raised when script is done. """
