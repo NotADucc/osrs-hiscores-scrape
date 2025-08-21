c54ae93923e0728c634077519bfedb5ba9ee8ac0
Guillaume Desktop <robertohelper@outlook.com>
2025-08-21 02:29:42 +0200

    feature docstrings for json_wrapper

--- src/util/json_wrapper.py
+++ src/util/json_wrapper.py
@@ -5,8 +5,28 @@ _json_lib = json
 
 
 def to_json(data: Any, **kwargs) -> str:
+    """
+    Serialize an object to a JSON-formatted string.
+
+    Args:
+        data (Any): The object to serialize.
+        **kwargs: Additional keyword arguments passed to `json.dumps`.
+
+    Returns:
+        str: The JSON-formatted string representation of `data`.
+    """
     return _json_lib.dumps(data, **kwargs)
 
 
 def from_json(json_string: str, **kwargs) -> Any:
+    """
+    Deserialize a JSON-formatted string into an object.
+
+    Args:
+        json_string (str): The JSON string to deserialize.
+        **kwargs: Additional keyword arguments passed to `json.loads`.
+
+    Returns:
+        Any: The resulting object.
+    """
     return _json_lib.loads(json_string, **kwargs)
