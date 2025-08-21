46717b6507566525989145e3a8f03d38f6ddfbce
Guillaume Desktop <robertohelper@outlook.com>
2025-08-21 02:51:04 +0200

    feature docstrings for mem_profile

--- src/util/mem_profile.py
+++ src/util/mem_profile.py
@@ -4,6 +4,14 @@ import psutil
 
 
 def memory_usage_psutil():
+    """
+    Get the current process memory usage in megabytes.
+
+    Uses `psutil` to retrieve the resident set size (RSS) of the current process.
+
+    Returns:
+        float: Memory usage in MB.
+    """
     process = psutil.Process(os.getpid())
     mem = process.memory_info().rss / (1024 ** 2)
     return mem
