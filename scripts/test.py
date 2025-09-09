6f9d30961d8a5c45f07b713809cf5e7330a0dcfc
Guillaume Desktop <robertohelper@outlook.com>
2025-09-09 14:00:37 +0200

    removed test file

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-from src.request.common import HSAccountTypes, HSType
-from src.util.io import build_temp_file
-
-print(build_temp_file(account_type=HSAccountTypes.regular, hs_type=HSType.agility,
-      file_path="D:\\Snogramming\\osrs-hiscore-scrape\\test.txt"))
-input()
