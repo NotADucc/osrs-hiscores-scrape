fbd9c7f7fef9ccae84c0babcbafaa6a0bd23918c
Guillaume Desktop <robertohelper@outlook.com>
2026-04-16 19:58:11 +0200

    refactor: messing around with python packages

---
+++ osrs_hiscore_scrape/__init__.py
@@ -0,0 +1,25 @@
+from .request import common, dto, errors, request, results
+from .stats import common
+from .util import benchmarking, io, json_wrapper, log, mem_profile, predicate_utils, retry_handler, script_utils
+from .worker import common, job, mappers, worker
+
+__all__ = [
+    "common",
+    "dto",
+    "errors",
+    "request",
+    "results",
+    "common",
+    "benchmarking",
+    "io",
+    "json_wrapper",
+    "log",
+    "mem_profile",
+    "predicate_utils",
+    "retry_handler",
+    "script_utils",
+    "common",
+    "job",
+    "mappers",
+    "worker",
+]
